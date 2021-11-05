6e068f26a04247fc211c99808ecf511ff8606bbb
jaymasurkar <jaymasurkar@ternaengg.ac.in>
2021-11-05 17:14:54 +0530

    fb functions index clean-up

--- functions/app.js
+++ functions/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const cors = require('cors')({origin: true});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
@@ -13,7 +13,7 @@ var adminRouter = require('./routes/admin');
 var testRouter = require('./routes/test');
 
 var app = express();
-
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
